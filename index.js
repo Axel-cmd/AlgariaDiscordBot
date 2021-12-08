0c504d16fbcd7fb1ed1b95265db5a8ee9f9e78a7
Axel-MRT <77530883+Axel-cmd@users.noreply.github.com>
2021-12-09 00:26:50 +0100

    Update index.js

--- index.js
+++ index.js
@@ -13,5 +13,25 @@ client.once('ready', ()=>{
     console.log('Bot is ready !');
 })
 
+
+/*****Ajouter les commandes  *****/
+
+client.on('interactionCreate', async interaction=>{
+    //vérifier si l'interaction est bien une commande avec la fonction isCommand()
+    if(!interaction.isCommand()) return;
+
+    //récupérer le nom de l'interaction dans la variable
+    const { commandName } = interaction;
+
+    if(commandName ==='ping'){
+        await interaction.reply('Pong!');
+    }else if (commandName === 'server'){
+        await interaction.reply('Server info.');
+    }else if(commandName === 'user'){
+        await interaction.reply('User info.')
+    }
+});
+
+
 //se connecter à discord avec le tokken
 client.login(TOKEN);
